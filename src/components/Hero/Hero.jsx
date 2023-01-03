3a467b4257ff84b16df5b1302b3abc4e267b89d8
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-01-03 22:40:46 +0000

    fix hero section responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./hero.scss";
 import rocket from "../../assets/collection/icons/RocketLaunch.svg";
 import hero from "../../assets/collection/Discover/Image Placeholder.png";
-
+import avatar from "../../assets/collection/Discover/Avatar Placeholder.svg";
 export default function Hero() {
 	return (
 		<div className="hero">
@@ -13,20 +13,20 @@ export default function Hero() {
 						NFT marketplace UI created with Anima for Figma. Collect, buy and
 						sell art from more than 20k NFT artists.
 					</p>
-					<button style={{ display: "none" }}>
+					<button className="button">
 						<img src={rocket} alt="get started" />
 						<p>Get started</p>
 					</button>
-					<div className="stats" style={{ display: "none" }}>
-						<div className="sale">
+					<div className="stats">
+						<div className="sale stat">
 							<h2>240k+</h2>
 							<span>Total Sale</span>
 						</div>
-						<div className="auctions">
+						<div className="auctions stat">
 							<h2>100k+</h2>
 							<span>Auctions</span>
 						</div>
-						<div className="artists">
+						<div className="artists stat">
 							<h2>240k+</h2>
 							<span>Artists</span>
 						</div>
@@ -34,11 +34,11 @@ export default function Hero() {
 				</div>
 				<div className="right-container">
 					<div className="card">
-						<img src={hero} alt="Animakid" />
+						<img className="anima" src={hero} alt="Animakid" />
 						<div className="bottom">
 							<h5>Space Walking</h5>
 							<div className="owner">
-								<div className="img"></div>
+								<img src={avatar} alt="animakid" />
 								<span>Animakid</span>
 							</div>
 						</div>
@@ -48,16 +48,16 @@ export default function Hero() {
 						<p>Get started</p>
 					</button>
 					<div className="stats">
-						<div className="sale">
-							<h2>240k+</h2>
+						<div className="sale stat">
+							<h5>240k+</h5>
 							<span>Total Sale</span>
 						</div>
-						<div className="auctions">
-							<h2>100k+</h2>
+						<div className="auctions stat">
+							<h5>100k+</h5>
 							<span>Auctions</span>
 						</div>
-						<div className="artists">
-							<h2>240k+</h2>
+						<div className="artists stat">
+							<h5>240k+</h5>
 							<span>Artists</span>
 						</div>
 					</div>
