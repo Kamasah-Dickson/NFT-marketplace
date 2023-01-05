b539e43fc7a6ea39021ca32b46acb3242be47f6e
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-01-05 23:50:44 +0000

    top creators section completed

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Categories() {
+	return (
+		<div className="categories-section">
+			<div className="container">
+				<h4>Browse Categories</h4>
+				<div className="browse-categories">
+					<div className="card">
+						<div className="img">
+							<img src="" alt="" />
+						</div>
+						<p className="category"></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Collections from "../../components/collections/collections";
 import Creators from "../../components/creators/creators";
+import Categories from "../../components/Categories/Categories";
 import "./home.scss";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
 			<Hero />
 			<Collections />
 			<Creators />
+			<Categories />
 		</main>
 	);
 }
