1e556eeae18481e0bc465ffb594058a4be5bc2ec
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-01-07 12:15:21 +0000

    add discover section

--- src/components/Discover-More/Discover.jsx
+++ src/components/Discover-More/Discover.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./discover.scss";
 import discover from "../../assets/collection/Discover/Image Placeholder-1.svg";
-
+import discover2 from "../../assets/collection/Discover/Image Placeholder-2.svg";
+import discover3 from "../../assets/collection/Discover/Image Placeholder.svg";
+import seen from "../../assets/collection/Discover/Eye.svg";
+import profile1 from "../../assets/collection/Discover/Avatar Placeholder-1.svg";
+import profile2 from "../../assets/collection/Discover/Avatar Placeholder-2.svg";
+import profile3 from "../../assets/collection/Discover/Avatar Placeholder.svg";
 export default function Discover() {
 	return (
 		<div className="discover-section">
@@ -13,7 +18,7 @@ export default function Discover() {
 					</div>
 					<div className="right">
 						<button>
-							<img className="seen" src="" alt="" />
+							<img className="seen" src={seen} alt="see all" />
 							<p>See All</p>
 						</button>
 					</div>
@@ -23,21 +28,77 @@ export default function Discover() {
 						<div className="img">
 							<img src={discover} alt="discover" />
 						</div>
-						<div className="bottom">
-							<h5 className="name"></h5>
-							<div className="user">
-								<div className="img"></div>
-								<p>See All</p>
+						<div className="bottom-card">
+							<div className="bottom">
+								<h5 className="name">Life On Edena</h5>
+								<div className="user">
+									<div className="img">
+										<img src={profile1} alt="nebula kid" />
+									</div>
+									<p>NebulaKid</p>
+								</div>
+							</div>
+							<div className="footer">
+								<div className="left">
+									<span>Price</span>
+									<p>1.63 ETH</p>
+								</div>
+								<div className="right">
+									<span>Highest Bid</span>
+									<p>0.33 wETH</p>
+								</div>
 							</div>
 						</div>
-						<div className="footer">
-							<div className="left">
-								<span>Price</span>
-								<p>1.63 ETH</p>
+					</div>
+					<div className="card">
+						<div className="img">
+							<img src={discover2} alt="discover" />
+						</div>
+						<div className="bottom-card">
+							<div className="bottom">
+								<h5 className="name">AstroFiction</h5>
+								<div className="user">
+									<div className="img">
+										<img src={profile2} alt="nebula kid" />
+									</div>
+									<p>Spaceone</p>
+								</div>
 							</div>
-							<div className="right">
-								<span>Highest Bid</span>
-								<p>0.33 wETH</p>
+							<div className="footer">
+								<div className="left">
+									<span>Price</span>
+									<p>1.63 ETH</p>
+								</div>
+								<div className="right">
+									<span>Highest Bid</span>
+									<p>0.33 wETH</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="card">
+						<div className="img">
+							<img src={discover3} alt="discover" />
+						</div>
+						<div className="bottom-card">
+							<div className="bottom">
+								<h5 className="name">Distant Galaxy</h5>
+								<div className="user">
+									<div className="img">
+										<img src={profile3} alt="nebula kid" />
+									</div>
+									<p>Spaceone</p>
+								</div>
+							</div>
+							<div className="footer">
+								<div className="left">
+									<span>Price</span>
+									<p>1.63 ETH</p>
+								</div>
+								<div className="right">
+									<span>Highest Bid</span>
+									<p>0.33 wETH</p>
+								</div>
 							</div>
 						</div>
 					</div>
